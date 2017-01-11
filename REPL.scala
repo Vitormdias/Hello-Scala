a5081686b1b71a1807fe8c515303fa4963142659
Vitor Manfré <vitormd97@gmail.com>
2017-01-10 23:50:55 -0200

    REPL example added

---
+++ REPL.scala
@@ -0,0 +1,14 @@
+//DEFINITION:
+
+/*REPL é um interpretador que lê uma entrada, avalia e executa essa entrada, e
+imprime o resultado, para então começar tudo de novo — o loop.*/
+
+//BASH scala
+
+// scala> 1 + 2
+// res0: Int = 3
+
+// ele cria a variavel res0
+
+//scala> val texto = "Numero": + res0
+//texto: String = Numero: 3
